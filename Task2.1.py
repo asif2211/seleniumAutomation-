8cf40e7733f31391413fc8a4abb7e6401dfcdff3
asif2211 <asifbsit@gmail.com>
2021-09-08 17:29:18 +0500

    test with proper class

--- Task2.1.py
+++ Task2.1.py
@@ -5,46 +5,79 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 #  Explicit wait for certain condition until not get exception of NotElementVisible
-import sys
+
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
-
-for p in sys.path:
-    print(p)
-# for Chrome driver
-# driver = webdriver.Chrome()
-#  First way is this
-# driver = webdriver.Firefox(executable_path="/Users/muhammadasif/Downloads/geckodriver")
-
-# 2nd way is install GeckoDriverManager and pass it in Firefox object
-
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-# driver = webdriver.Firefox()
-driver.get("https://demoqa.com/text-box")
-assert "ToolsQA" in driver.title
-wait = WebDriverWait(driver, 10)
-# Full Name
-full_name = wait.until(EC.presence_of_element_located  ((By.CSS_SELECTOR, 'input[placeholder="Full Name"]')))
-full_name.clear()
-full_name.send_keys("MuhammadAsif")
-
-# Email
-email = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="name@example.com"]')))
-email.clear()
-email.send_keys("abc@xyz.com")
+from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 
-# Addresss
-address = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea#currentAddress')))
-address.clear()
-address.send_keys("asif from lahroe")
+class SeleniumTestTask2 :
+
+    def chromeTest(self):
+
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        driver.get("https://demoqa.com/text-box")
+
+        assert "ToolsQA" in driver.title
+        wait = WebDriverWait(driver, 10)
+        # Full Name
+        full_name = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Full Name"]')))
+        full_name.clear()
+        full_name.send_keys("MuhammadAsif")
+
+        # Email
+        email = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="name@example.com"]')))
+        email.clear()
+        email.send_keys("abc@xyz.com")
+
+        # Addresss
+        address = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea#currentAddress')))
+        address.clear()
+        address.send_keys("asif from lahroe")
+
+        # Permanent Address
+        per_address = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea#permanentAddress')))
+        per_address.clear()
+        per_address.send_keys("Asif From khanpur")
+
+        driver.execute_script(
+            FireEvents.fire_event_script + "fireEvent(document.querySelector('button#submit'), 'click');")
+        time.sleep(2)
+        driver.close()
+
+    def fireFoxTest(self):
+
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        driver.get("https://demoqa.com/text-box")
+        assert "ToolsQA" in driver.title
+        wait = WebDriverWait(driver, 10)
+        # Full Name
+        full_name = wait.until(EC.presence_of_element_located  ((By.CSS_SELECTOR, 'input[placeholder="Full Name"]')))
+        full_name.clear()
+        full_name.send_keys("MuhammadAsif")
+
+        # Email
+        email = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="name@example.com"]')))
+        email.clear()
+        email.send_keys("abc@xyz.com")
+
+        # Addresss
+        address = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea#currentAddress')))
+        address.clear()
+        address.send_keys("asif from lahroe")
+
+        # Permanent Address
+        per_address = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea#permanentAddress')))
+        per_address.clear()
+        per_address.send_keys("Asif From khanpur")
+
+        driver.execute_script(FireEvents.fire_event_script + "fireEvent(document.querySelector('button#submit'), 'click');")
+        time.sleep(2)
+        driver.close()
 
 
-# Permanent Address
-per_address = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea#permanentAddress')))
-per_address.clear()
-per_address.send_keys("Asif From khanpur")
-
-driver.execute_script(FireEvents.fire_event_script + "fireEvent(document.querySelector('button#submit'), 'click');")
-time.sleep(2)
-driver.close()
+testObject = SeleniumTestTask2()
+testObject.chromeTest()
+# testObject.fireFoxTest()
--- Task2.2.py
+++ Task2.2.py
@@ -4,33 +4,75 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 
-driver = webdriver.Firefox(executable_path="/Users/muhammadasif/Downloads/geckodriver")
-driver.get("https://demoqa.com/checkbox")
+class SeleniumTestTask2 :
+    def chromeTest(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get("https://demoqa.com/checkbox")
 
-assert "ToolsQA" in driver.title
+        assert "ToolsQA" in driver.title
 
-wait = WebDriverWait(driver, 20)
+        wait = WebDriverWait(driver, 20)
 
-Expand_check_box = wait.until(EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, '.rct-icon.rct-icon-expand-all')))
+        Expand_check_box = wait.until(EC.visibility_of_element_located(
+                        (By.CSS_SELECTOR, '.rct-icon.rct-icon-expand-all')))
 
-Expand_check_box.click()
+        Expand_check_box.click()
 
-#  Checked here
+        #  Checked here
 
-Checked = wait.until(EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, 'span.rct-checkbox')))
+        Checked = wait.until(EC.visibility_of_element_located(
+                        (By.CSS_SELECTOR, 'span.rct-checkbox')))
 
-Checked.click()
+        Checked.click()
 
-#  Unchecked here
+        #  Unchecked here
 
-Checked = wait.until(EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, 'span.rct-checkbox')))
+        Checked = wait.until(EC.visibility_of_element_located(
+                        (By.CSS_SELECTOR, 'span.rct-checkbox')))
 
-Checked.click()
+        Checked.click()
 
-print('test is finished')
-driver.close()
+        print('test is finished')
+        driver.close()
+
+    def firefoxTest(self):
+
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        driver.get("https://demoqa.com/checkbox")
+
+        assert "ToolsQA" in driver.title
+
+        wait = WebDriverWait(driver, 20)
+
+        Expand_check_box = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, '.rct-icon.rct-icon-expand-all')))
+
+        Expand_check_box.click()
+
+        #  Checked here
+
+        Checked = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, 'span.rct-checkbox')))
+
+        Checked.click()
+
+        #  Unchecked here
+
+        Checked = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, 'span.rct-checkbox')))
+
+        Checked.click()
+
+        print('test is finished')
+        driver.close()
+
+
+testObject = SeleniumTestTask2()
+testObject.chromeTest()
+# testObject.firefoxTest()
